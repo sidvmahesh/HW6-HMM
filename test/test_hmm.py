3bd0c8c003ea30de534f87e20476a179dc84636e
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-02-22 22:04:43 -0800

    final changes

--- test/test_hmm.py
+++ test/test_hmm.py
@@ -21,21 +21,32 @@ def test_mini_weather():
 
     mini_hmm=np.load('./data/mini_weather_hmm.npz')
     mini_input=np.load('./data/mini_weather_sequences.npz')
-    #print(mini_hmm)
     hmm = HiddenMarkovModel(mini_hmm["observation_states"], mini_hmm["hidden_states"], mini_hmm["prior_p"], mini_hmm["transition_p"], mini_hmm["emission_p"])
-    print("hmm.forward(mini_input[observation_state_sequence]): ", hmm.forward(mini_input["observation_state_sequence"]))
-    #print(mini_input)
-    print("hmm.viterbi(mini_input[observation_state_sequence]): ", hmm.viterbi(mini_input["observation_state_sequence"]))
-    print(mini_input["best_hidden_state_sequence"])
+    forward_result =  hmm.forward(mini_input["observation_state_sequence"])
 
+    # Now, test the forward result with the ground truth
+    input_observation_states = mini_input["observation_state_sequence"]
+    num_obs = len(input_observation_states)
+    num_states = len(hmm.hidden_states)
+    probs = np.zeros((num_obs, num_states))
+    for i in range(num_states):
+        probs[0, i] = hmm.prior_p[i] * hmm.emission_p[i][hmm.observation_states_dict[input_observation_states[0]]]
+    for t in range(1, num_obs):
+        for j in range(num_states):
+            for i in range(num_states):
+                probs[t, j] += probs[t-1, i] * hmm.transition_p[i][j] * hmm.emission_p[j][hmm.observation_states_dict[input_observation_states[t]]]
+    ground_truth_result = np.sum(probs[num_obs - 1, :])
 
-
-
-
-
+    assert ground_truth_result == forward_result
     
-   
-    pass
+    #print("hmm.viterbi(mini_input[observation_state_sequence]): ", hmm.viterbi(mini_input["observation_state_sequence"]))
+    #print(mini_input["best_hidden_state_sequence"])
+    
+    #Now, test the viterbi result
+    viterbi_result = hmm.viterbi(mini_input["observation_state_sequence"])
+    assert len(viterbi_result) == len(mini_input["best_hidden_state_sequence"])
+    for i in range(len(viterbi_result)):
+        assert viterbi_result[i] == mini_input["best_hidden_state_sequence"][i]
 
 
 
@@ -50,9 +61,17 @@ def test_full_weather():
     Assert that the state sequence returned is in the right order, has the right number of states, etc. 
 
     """
-
-    pass
-
+    full_hmm=np.load('./data/full_weather_hmm.npz')
+    full_input=np.load('./data/full_weather_sequences.npz')
+    hmm = HiddenMarkovModel(full_hmm["observation_states"], full_hmm["hidden_states"], full_hmm["prior_p"], full_hmm["transition_p"], full_hmm["emission_p"])
+    print("\nforward_result: ", hmm.forward(full_input["observation_state_sequence"]))
+    #print("hmm.forward(mini_input[observation_state_sequence]): ", hmm.forward(full_input["observation_state_sequence"]))
+    #print("hmm.viterbi(mini_input[observation_state_sequence]): ", hmm.viterbi(full_input["observation_state_sequence"]))
+    #print(full_input["best_hidden_state_sequence"])
+    viterbi_result = hmm.viterbi(full_input["observation_state_sequence"])
+    assert len(viterbi_result) == len(full_input["best_hidden_state_sequence"])
+    for i in range(len(viterbi_result)):
+        assert viterbi_result[i] == full_input["best_hidden_state_sequence"][i]
 
 
 
